09cd9369c4099d6767d13acd9819b1c86948f393
Andrew Lee <git@drewlee.com>
2016-08-27 18:28:30 -0700

    Links toggle content

---
+++ js/site.js
@@ -0,0 +1,37 @@
+jQuery(function($) {
+  var i = 0;
+  var divs = $('div.content');
+  var div_count = divs.length;
+  var div;
+
+  function hide_divs(){
+    for (i = 0; i < div_count; i++) {
+      div = $(divs[i]);
+      if (!div.hasClass('hidden')) {
+        div.addClass('hidden');
+      }
+    }
+  };
+
+  hide_divs();
+
+  $('#atm-ucla').click(function(e) {
+    e.preventDefault();
+    hide_divs();
+    $('#div-atm-ucla').removeClass('hidden');
+  });
+
+  $('#wid-work').click(function(e) {
+    e.preventDefault();
+    hide_divs();
+    $('#div-wid-work').removeClass('hidden');
+  });
+
+  $('#lc-email').click(function(e) {
+    e.preventDefault();
+    hide_divs();
+    $('#div-lc-email').removeClass('hidden');
+  });
+
+
+});
